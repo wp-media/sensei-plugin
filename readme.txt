c0ef793c4a92b4b5d4297ffec1e90baae2a84328
Jean-Baptiste Marchand-Arvier <jb.marchandarvier@gmail.com>
2016-06-24 14:43:06 +0200

    First commit

---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== Sensei plugin ===
+Contributors: wp_media
+Requires at least: 3.7.0
+Tested up to: 4.5.2
+Stable tag: 1.5.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Sensei test plugin
---
+++ sensei-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Sensei Plugin
+
+Description: Sensei plugin for test good practice
+Version: 1.0
+Author: WP Media
+Author URI: http://wp-media.me
+Licence: GPLv2
+
+
+Copyright 2016 WP Media
+*/
+
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+//  defines
+define( 'SENSEIN_PLUGIN_VERSION'	 			 , '1.0' );
+
