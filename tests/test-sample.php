f363408965ffd516429db73bee752d2324740ed6
Jean-Baptiste Marchand-Arvier <jb.marchandarvier@gmail.com>
2016-06-28 15:50:45 +0200

    Fixing assert

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,6 +1,6 @@
 <?php
 class SampleTest extends WP_UnitTestCase {
     function testSample() {
-        $this->assertFalse( is_plugin_active('sensei-plugin/sensei-plugin.php') );
+        $this->assertTrue( is_plugin_active('sensei-plugin/sensei-plugin.php') );
     }
 }
